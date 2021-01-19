0a7d32ae5dd8d80201369c33ec23917f133dc0fa
Richard <richardjalba@gmail.com>
2021-01-19 12:21:00 -0800

    Implemented Socket.io

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
-  res.send('Chat App!');
+io.on('connection', () => {
+  console.log('websocket connected');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
