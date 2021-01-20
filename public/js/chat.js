fbb3c20fa8ab95fef742cf844273f64a5ae14548
Richard <richardjalba@gmail.com>
2021-01-20 10:55:55 -0800

    Added clg messaging fx

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = document.querySelector('#message').value;
+  console.log('Message Sent');
+  socket.emit('sendMsg', message);
+});
--- src/index.js
+++ src/index.js
@@ -12,8 +12,14 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('websocket connected');
+
+  socket.emit('message', 'Welcome to Dodo Communications.');
+
+  socket.on('sendMsg', (message) => {
+    io.emit('message', message);
+  });
 });
 
 server.listen(port, () => {
